45c931a7ba30e527363824dd3b14631eb7f57bbb
kauzkoko <kamelkebab@tutanota.com>
2024-12-18 01:43:57 +0900

    test2

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="outer">
+  <div class="outer-landscape" v-if="landscapeMobile">
+    <div class="container">
+      <template v-for="block in data?.contents">
+        <img v-if="block.image" :src="block.image.display.url" />
+      </template>
+    </div>
+  </div>
+  <div class="outer" v-else>
     <div class="container">
       <template v-for="block in data?.contents">
         <img v-if="block.image" :src="block.image.display.url" />
@@ -9,6 +16,9 @@
 </template>
 
 <script setup>
+const { width, height } = useWindowSize();
+const landscapeMobile = computed(() => width.value / height.value > 1.7 && width.value < 1024);
+
 const { data } = await useFetch(
   "https://api.are.na/v2/channels/artworks-i-took-a-picture-of",
   {
@@ -30,34 +40,52 @@ body {
   width: 100vw;
   display: flex;
   justify-content: center;
+
+  .container {
+    padding: 20px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+
+    @media (min-width: 1024px) {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr;
+      gap: 10px;
+    }
+
+    @media (min-width: 1660px) {
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+    }
+
+    img {
+      width: 100%;
+      height: 500px;
+      background-color: blue;
+
+      @media (max-width: 1024px) {
+        margin: 10px;
+      }
+    }
+  }
 }
 
-.container {
-  padding: 20px;
+.outer-landscape {
   display: flex;
+  flex-direction: row;
+  height: 100vh;
   align-items: center;
-  justify-content: center;
-  flex-direction: column;
 
-  @media (min-width: 1024px) {
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    gap: 10px;
-  }
+  .container {
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
 
-  @media (min-width: 1660px) {
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
-  }
-}
-
-img {
-  width: 100%;
-  height: 500px;
-  background-color: blue;
-  
-
-  @media (max-width: 1024px) {
-    margin: 10px;
+    img {
+      margin-right: 20px;
+      width: 90vw;
+      height: 90vh;
+    }
   }
 }
 </style>
